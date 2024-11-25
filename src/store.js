9fc8e69c737ad5ee72519925950a6ab24991ca59
macdallas123 <roylexstephane@gmail.com>
2024-11-25 07:29:55 +0100

    correct errors due to last git merge

--- src/store.js
+++ src/store.js
@@ -16,21 +16,3 @@ const store = configureStore({
 });
 
 export default store;
-import {configureStore} from "@reduxjs/toolkit";
-import { apiSlice } from "./slices/apiSlice";
-import authReducer from "./slices/auth/authSlice";
-
-import {disableReactDevTools} from "@fvilers/disable-react-devtools";
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools;
-
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-   [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: false,
-});
-
-export default store;
