5659c1fe72c9af0fb8da7ca3a290b54b9157b574
mimlyrics <mimlyrics8@gmail.com>
2024-11-26 18:26:44 +0100

    update last5

--- src/components/admin/sales/AdminEditSale.jsx
+++ src/components/admin/sales/AdminEditSale.jsx
@@ -37,7 +37,7 @@ const AdminEditSale = () => {
       const {searchId} =  queryString.parse(location.search);
       setIdx(searchId);
       console.log(idx);
-      console.log(searchId);
+      console.log("searchId :" + searchId);
     }, [idx])
     
 
