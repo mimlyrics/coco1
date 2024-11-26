d39ee153f6d7b5c015d5b04b091411507922c9d8
mimlyrics <mimlyrics8@gmail.com>
2024-11-26 15:49:57 +0100

    update last5

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import App from "./App.jsx";
 import store from "./store.js";
 import { Provider } from "react-redux";
 import AppProvider from "./components/context/AppProvider.jsx";
-import { HashRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>  
-      <HashRouter basename="/">
         <AppProvider>
           <App />
         </AppProvider>
-      </HashRouter>
-
     </Provider>
   </React.StrictMode>
 );
