7f3826fe06aaf069a3090551d71491f3be467049
mimlyrics <mimlyrics8@gmail.com>
2024-11-26 15:46:10 +0100

    update last5

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,12 @@ import { HashRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>  
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <HashRouter basename="/">
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </HashRouter>
+
     </Provider>
   </React.StrictMode>
 );
